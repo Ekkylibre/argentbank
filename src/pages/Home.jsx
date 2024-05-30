9058c5230ff0c26a1eabc6b8c99521bd2d8d65b5
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-30 19:59:12 +0200

    ✨ Components > Navbar > Create Navbar component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,9 @@
-import { Link } from 'react-router-dom';
+import Navbar from "../components/NavBar";
 
 function Home() {
   return (
     <>
-      <nav className="main-nav">
-        <Link className="main-nav-logo" to="/">
-          <img
-            className="main-nav-logo-image"
-            src="./img/argentBankLogo.png"
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </Link>
-        <div>
-          <Link className="main-nav-item" to="/sign-in">
-            <i className="fa fa-user-circle"></i>
-            Sign In
-          </Link>
-        </div>
-      </nav>
+      <Navbar />
       <main>
         <div className="hero">
           <section className="hero-content">
