f3ea5ebdd12aead359d9b27e98739ac382dd4dee
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-30 19:18:20 +0200

    ✨ Pages > User > Create User page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./app.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
+import UserPage from "./pages/User";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/user" element={<UserPage />} />
     </Routes>
   );
 }
