aa4aaa1a5af96fa9178cc76aa8d487b7e752c51d
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-05-30 19:13:12 +0200

    ✨ Components > SignIn > Create SignIn page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import Home from "./components/Home"
 import "./app.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import SignIn from "./components/SignIn";
 
 function App() {
-
   return (
-    <>
-       <Home />
-    </>
-  )
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sign-in" element={<SignIn />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,22 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
   return (
     <>
       <nav className="main-nav">
-        <a className="main-nav-logo" href="./index.html">
+        <Link className="main-nav-logo" to="/">
           <img
             className="main-nav-logo-image"
             src="./img/argentBankLogo.png"
             alt="Argent Bank Logo"
           />
           <h1 className="sr-only">Argent Bank</h1>
-        </a>
+        </Link>
         <div>
-          <a className="main-nav-item" href="./sign-in.html">
+          <Link className="main-nav-item" to="/sign-in">
             <i className="fa fa-user-circle"></i>
             Sign In
-          </a>
+          </Link>
         </div>
       </nav>
       <main>
