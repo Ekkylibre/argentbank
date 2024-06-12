3623fb91084b4f571125e2a4e5448de4b410a7f4
Dany XIONG <dany.xiong.dev@outlook.fr>
2024-06-12 14:07:19 +0200

    ✨ Components > Hero, Features, FeatureItem > Create components

---
+++ src/components/FeatureItem.jsx
@@ -0,0 +1,11 @@
+function FeatureItem({ imgSrc, imgAlt, title, children }) {
+    return (
+      <div className="feature-item">
+        <img src={imgSrc} alt={imgAlt} className="feature-icon" />
+        <h3 className="feature-item-title">{title}</h3>
+        <p>{children}</p>
+      </div>
+    );
+  }
+  
+  export default FeatureItem;
